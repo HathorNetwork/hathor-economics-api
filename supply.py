35dda287f8016441d7209c182e61b65c937a7995
Pedro Ferreira <pedro@hathor.network>
2020-05-06 21:48:06 -0300

    feat: adding decimals filter for total supply endpoint

--- supply.py
+++ supply.py
@@ -7,21 +7,8 @@ from constants import BASE_PAYLOAD, DECIMAL_PLACES
 from util import get_economics_status, get_status_default_error
 
 
-def total_supply(event: Dict[str, Any], context: 'bootstrap.LambdaContext') -> Dict[str, Any]:
-    """ Return the total supply getting data from the status lambda function
-    """
-    status = get_economics_status()
-    if not status:
-        # In case of error we return the default error
-        return get_status_default_error()
-
-    payload = BASE_PAYLOAD
-    payload['body'] = status['total_supply']
-    return payload
-
-
-def circulating_supply(event: Dict[str, Any], context: 'bootstrap.LambdaContext') -> Dict[str, Any]:
-    """ Return the circulating supply getting data from the status lambda function
+def get_payload(key: str, event: Dict[str, Any]) -> Dict[str, Any]:
+    """ Return the payload for the request depending on the key (total_supply or circulating_supply)
     """
     status = get_economics_status()
     if not status:
@@ -30,16 +17,30 @@ def circulating_supply(event: Dict[str, Any], context: 'bootstrap.LambdaContext'
 
     payload = BASE_PAYLOAD
 
-    circulating_supply = status['circulating_supply']
-    payload['body'] = circulating_supply
+    value = status.get(key)
+    assert value is not None
+
+    payload['body'] = value
 
     queryStringParams = event.get('queryStringParameters')
     if queryStringParams and queryStringParams.get('decimals') == 'true':
         assert sys.version_info >= (3, 6)
         # If we have decimals: true in the parameter, return as decimal
-        circulating_supply /=  10**DECIMAL_PLACES
+        value /=  10**DECIMAL_PLACES
         # This fixes the case where the decimal places are .00, so we must return with all zeros
-        circulating_supply_str = '%.{}f'.format(DECIMAL_PLACES) % circulating_supply
-        payload['body'] = circulating_supply_str
+        value_str = '%.{}f'.format(DECIMAL_PLACES) % value
+        payload['body'] = value_str
 
     return payload
+
+
+def total_supply(event: Dict[str, Any], context: 'bootstrap.LambdaContext') -> Dict[str, Any]:
+    """ Return the total supply getting data from the status lambda function
+    """
+    return get_payload('total_supply', event)
+
+
+def circulating_supply(event: Dict[str, Any], context: 'bootstrap.LambdaContext') -> Dict[str, Any]:
+    """ Return the circulating supply getting data from the status lambda function
+    """
+    return get_payload('circulating_supply', event)
