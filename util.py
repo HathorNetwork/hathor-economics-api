32a51f44c315f7df59f88645c9b0563e853dc863
Pedro Ferreira <pedro@hathor.network>
2020-07-07 23:37:26 -0300

    feat: adding status api with decimals parameter and thousand separator

--- util.py
+++ util.py
@@ -1,13 +1,14 @@
 import datetime
 import requests
+import sys
 
 from os import path
 from typing import Any, Dict
 
-from constants import BASE_PAYLOAD, RELEASED_PREMINED_TOKENS, SERVER, TOTAL_PREMINED
+from constants import BASE_PAYLOAD, DECIMAL_PLACES, RELEASED_PREMINED_TOKENS, SERVER, TOTAL_PREMINED
 
 
-def get_economics_status() -> Dict[str, Any]:
+def get_economics_status(decimals: bool, add_comma_separator: bool) -> Dict[str, Any]:
     """ Get economics status data
     """
     status = None
@@ -23,13 +24,29 @@ def get_economics_status() -> Dict[str, Any]:
         status = {
             'latest_update': int(datetime.datetime.now().timestamp()),
             'backend': SERVER,
-            'total_supply': total_supply,
-            'circulating_supply': circulating_supply,
-            'mined_tokens': mined_tokens,
-            'released_premined_tokens': RELEASED_PREMINED_TOKENS
+            'total_supply':
+                get_decimals(total_supply, add_comma_separator) if decimals else total_supply,
+            'circulating_supply':
+                get_decimals(circulating_supply, add_comma_separator) if decimals else circulating_supply,
+            'mined_tokens':
+                get_decimals(mined_tokens, add_comma_separator) if decimals else mined_tokens,
+            'released_premined_tokens':
+                get_decimals(RELEASED_PREMINED_TOKENS, add_comma_separator) if decimals else RELEASED_PREMINED_TOKENS,
         }
 
     return status
+
+
+def get_decimals(value: int, add_comma_separator: bool) -> str:
+    assert sys.version_info >= (3, 6)
+    value /= 10**DECIMAL_PLACES
+    # This fixes the case where the decimal places are .00, so we must return with all zeros
+    string_formatter = '{:.{}f}'
+    if add_comma_separator:
+        # Adding comma as thousands separator to string formatter
+        string_formatter = string_formatter[:2] + ',' + string_formatter[2:]
+
+    return string_formatter.format(value, DECIMAL_PLACES)
 
 
 def get_status_default_error() -> Dict[str, Any]:
